d45e8d08dbc276a15efcc4502b04ef627a6b07e8
Katie201677 <63996368+Katie201677@users.noreply.github.com>
2020-12-27 20:34:21 +0000

    Initial amends to make mobile-friendly.

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 const button = document.querySelector('.photo-container');
 
 function rotatePhoto() {
-//     let zindex = button.style.zIndex;
     let photos = button.querySelectorAll('.photo');
-        for (let i=0; i<photos.length; i++) {
-            let z = window.getComputedStyle(photos[i]).zIndex * 1;
-            if (z === 3) {
-                photos[i].style.zIndex = 1;
-            } else {
-                photos[i].style.zIndex = z + 1;
-            }
-            console.log(i, photos[i].style.zIndex);
-        }
-        
     console.log(photos);
+    for (let i=0; i<photos.length; i++) {
+        let z = window.getComputedStyle(photos[i]).zIndex * 1;
+        if (z === photos.length) {
+            photos[i].style.zIndex = 1;
+        } else {
+            photos[i].style.zIndex = z + 1;
+        }
+    }
 }
 
 button.addEventListener('click', rotatePhoto);
